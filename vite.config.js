3206e81d05ff729dab460f9be675008ec2ea32c2
Gourav.Goswami <gourav.goswami@decipherzone.com>
2025-08-18 23:00:15 +0530

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['axios']
-    },
-    outDir: 'dist',
-  }
-});
+// https://vitejs.dev/config/
+export default defineConfig(({mode}) =>{
+  const env = loadEnv(mode, process.cwd(), '')
+   return {
+   plugins: [react()],
+   build: { chunkSizeWarningLimit: 1200, },
+   }
+ });
